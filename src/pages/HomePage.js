57ff5bcbd9b6f92eaf8cefd3b986d1a33f8d6099
Han  Solo <hansolo@Death-Star.local>
2021-11-30 11:43:44 -0700

    final files

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const HomePage = () => (
   <>
-   <h1 id="home_page">Hello, welcome to Trails!</h1>
+   <h1 id="home_page">Welcome to our Trails map for CS 4420!</h1>
+      <h2>Our team consists of 5 individuals: Elizabeth Gavino, Norman Marroquin, Jonathan Contreras, and Trey McKinstry.</h2>
+      <p>In this site you will find information on hiking trails in Colorado Springs Colorado. The Trails tab above contains a list of the trails we have
+      identified on the map. When a trail is clicked more information is provided. The map tab contains trails we found in Colorado Springs. When
+      you hoover over a node, more information on the trails appears. The information includes the name, length of the trail, and coordinates.</p>
   </>
 );
 
